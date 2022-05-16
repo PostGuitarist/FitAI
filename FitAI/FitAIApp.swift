0db32ae80bd7666122c06a4ab957642c377b95f0
Zaden Connell <zaden.connell@gmail.com>
2022-05-15 21:19:49 -0400

    Initial Commit

---
+++ FitAI/FitAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitAIApp.swift
+//  FitAI
+//
+//  Created by Zaden Connell on 5/15/22.
+//
+
+import SwiftUI
+
+@main
+struct FitAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
